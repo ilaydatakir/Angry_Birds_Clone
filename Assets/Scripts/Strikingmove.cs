c6f87b15f8374612cbcbfe665d98bdbb668bb200
ILAYDA TAKIR <99023275+ilaydatakir@users.noreply.github.com>
2026-03-23 14:56:07 +0100

    add striking move

---
+++ Assets/Scripts/Strikingmove.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BirdLauncher2D : MonoBehaviour
+{
+    public float maxPullDistance = 3f;
+    public float launchForce = 500f;
+
+    private Vector2 startPosition;
+    private bool isDragging = false;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPosition = rb.position;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 pullVector = mousePos - startPosition;
+
+            if (pullVector.magnitude > maxPullDistance)
+                pullVector = pullVector.normalized * maxPullDistance;
+
+            rb.position = startPosition + pullVector;
+        }
+
+        if (isDragging && !Mouse.current.leftButton.isPressed)
+        {
+            isDragging = false;
+            Vector2 launchVector = startPosition - rb.position;
+            rb.isKinematic = false;
+            rb.AddForce(launchVector * launchForce);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        rb.isKinematic = true;
+    }
+}
