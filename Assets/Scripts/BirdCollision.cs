df269bb2556602483530605465a49c6644450928
ILAYDA TAKIR <99023275+ilaydatakir@users.noreply.github.com>
2026-03-27 15:42:02 +0100

    remove unused pig

--- Assets/Scripts/BirdCollision.cs
+++ Assets/Scripts/BirdCollision.cs
@@ -6,7 +6,6 @@ public class BirdCollision : MonoBehaviour
     {
         string name = collision.gameObject.name;
 
-        // Sadece ahşap kutular ve domuzlar için kamera sallansın
         if (name.Contains("pig") || name.Contains("Wood") || name.Contains("wood"))
         {
             CameraShake shake = Camera.main.GetComponent<CameraShake>();
